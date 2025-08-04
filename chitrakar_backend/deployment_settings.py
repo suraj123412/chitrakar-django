34f942e70b854631883cdd94dd22e84a69194127
SurajWebM <surajbuss08@gmail.com>
2025-08-04 10:37:17 +0530

    changed settings

---
+++ chitrakar_backend/deployment_settings.py
@@ -0,0 +1,44 @@
+import os
+import dj_database_url
+from .settings import *
+from .settings import BASE_DIR
+
+ALLOWED_HOSTS = [os.environ.get['RENDER_EXTERNAL_HOSTNAME']]
+CSRF_TRUSTED_ORIGIN = ['https://'+os.environ.get['RENDER_EXTERNAL_HOSTNAME']]
+
+DEBUG = False
+SECRET_KEY = os.environ.get['secret key']
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+]
+
+# CORS_ALLOWED_ORIGINS= [
+#     'http://localhost:5173'
+# ]
+
+STORAGES = {
+    "default":{
+        "BACKEND" : "django.core.files.storage.FileSystemStorage",
+        
+    },
+    "staticfiles" : {
+        "BACKEND": "whitenoise.storage.CompressedStaticFileStorage",
+        
+    },
+}
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get['DATABASE_URL'],
+        conn_max_age=600
+    )
+}
