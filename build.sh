f99a596267e8e25e10cb337ff132fd44405379f6
SurajWebM <surajbuss08@gmail.com>
2025-08-04 12:32:46 +0530

    Deployment SUPERUSER

--- build.sh
+++ build.sh
@@ -6,7 +6,6 @@ python manage.py collectstatic --no-input
 
 python manage.py migrate
 
-if [[$CREATE_SUPERUSER]];
-then 
-     python manage.py createsuperuser --no-input
+if [ "$CREATE_SUPERUSER" = "true" ]; then
+    python manage.py createsuperuser --no-input
 fi
