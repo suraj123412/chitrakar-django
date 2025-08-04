287bd661174b46337fdeef17faf45b6afb337a9f
SurajWebM <surajbuss08@gmail.com>
2025-08-04 11:37:06 +0530

    Deployment setting secretkey

--- chitrakar_backend/deployment_settings.py
+++ chitrakar_backend/deployment_settings.py
@@ -38,7 +38,7 @@ STORAGES = {
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get['DATABASE_URL'],
+        default = os.environ.get('DATABASE_URL'),
         conn_max_age=600
     )
 }
