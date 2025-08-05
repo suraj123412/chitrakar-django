459b84e8ee94eb3351a408f8378e42b77811af27
SurajWebM <surajbuss08@gmail.com>
2025-08-05 17:50:34 +0530

    Image Setting update

--- chitrakar_backend/urls.py
+++ chitrakar_backend/urls.py
@@ -33,4 +33,5 @@ urlpatterns = [
     path('api/order/', include('order.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
